6100a396e055ada2a08e51a8c77f3d33e1246cf2
Luka <lukaradovic96@gmail.com>
2020-06-04 02:03:12 +0200

    Actuator Device Sensor

--- app/src/main/java/com/has/data/GetData.java
+++ app/src/main/java/com/has/data/GetData.java
@@ -1,19 +1,35 @@
 package com.has.data;
 
+import com.has.model.Actuator;
 import com.has.model.Device;
+import com.has.model.Sensor;
 import com.has.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface GetData {
 
-    @GET("users/1")
-    Call<User> getUser();
+    @GET("users/{id}")
+    Call<User> getUser(@Path("id") String id);
+
+    //DEVICES
+
+    @GET("devices")
+    Call<Device> getDevices();
+
+    @GET("devices/{id}")
+    Call<Device> getDevice(@Path("id") String id);
+
+    @DELETE("devices/{id}")
+    Call<Device> deleteDevice(@Path("id") String id);
 
     @POST("devices")
     @FormUrlEncoded
@@ -23,6 +39,70 @@ public interface GetData {
                             @Field("versionTimestamp") Long versionTimestamp,
                             @Field("userId") Long userId);
 
+    @PUT("devices/{id}")
+    @FormUrlEncoded
+    Call<Void> updateDevice(@Field("name") String name,
+                            @Field("description") String description,
+                            @Field("versionTimestamp") Long versionTimestamp,
+                            @Field("userId") Long userId);
+
+    //ACTUATORS
+
+    @GET("actuators")
+    Call<Device> getActuators();
+
+    @GET("actuators/{id}")
+    Call<Actuator> getActuator(@Path("id") String id);
+
+    @DELETE("actuators/{id}")
+    Call<Actuator> deleteActuator(@Path("id") String id);
+
+    @POST("actuators")
+    @FormUrlEncoded
+    Call<Void> createActuator(@Field("id") Long id,
+                            @Field("reference") String reference,
+                            @Field("description") String description,
+                            @Field("value") String value,
+                            @Field("deviceId") Long deviceId);
+
+    @PUT("actuators/{id}")
+    @FormUrlEncoded
+    Call<Void> updateActuator(@Field("reference") String reference,
+                              @Field("description") String description,
+                              @Field("value") String value,
+                              @Field("deviceId") Long deviceId);
+
+
+    //SENSORS
+
+    @GET("sensors")
+    Call<Sensor> getSensors();
+
+    @GET("sensors/{id}")
+    Call<Sensor> getSensor(@Path("id") String id);
+
+    @DELETE("sensors/{id}")
+    Call<Sensor> deleteSensor(@Path("id") String id);
+
+    @POST("sensors")
+    @FormUrlEncoded
+    Call<Void> createSensor(@Field("id") Long id,
+                              @Field("reference") String reference,
+                              @Field("description") String description,
+                              @Field("value") String value,
+                            @Field("timestamp") Long timestamp,
+                            @Field("deviceId") Long deviceId);
+
+    @PUT("sensors/{id}")
+    @FormUrlEncoded
+    Call<Void> updateSensor(@Field("reference") String reference,
+                              @Field("description") String description,
+                              @Field("value") String value,
+                            @Field("timestamp") Long timestamp,
+                            @Field("deviceId") Long deviceId);
+
+
+    //USER
     @POST("user/login")
     @FormUrlEncoded
     Call<Void> login(@Field("email") String email, @Field("password") String password);
