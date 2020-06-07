9afe8eac0431cd2e881cb5f4d975ad2c0da3fe98
Luka <lukaradovic96@gmail.com>
2020-06-07 23:29:00 +0200

    Rules

--- app/src/main/java/com/has/RulesActivity.java
+++ app/src/main/java/com/has/RulesActivity.java
@@ -2,11 +2,14 @@ package com.has;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +17,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.has.adapters.RuleAdapter;
+import com.has.async.PopulateDevices;
+import com.has.data.DatabaseManager;
 import com.has.model.Rule;
 
 import java.util.ArrayList;
@@ -22,6 +27,11 @@ import java.util.List;
 public class RulesActivity extends BaseDrawerActivity {
 
     private List<Rule> ruleList = new ArrayList<>();
+    private DatabaseManager dbManager;
+    private RecyclerView.Adapter ruleAdapter;
+    private Long currentUserId;
+    private RecyclerView ruleRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +42,17 @@ public class RulesActivity extends BaseDrawerActivity {
 
         drawer.addView(contentView, 0);
         navigationView.setCheckedItem(R.id.nav_activity2);
-
-        RecyclerView ruleRecyclerView = findViewById(R.id.recycler_view_rules_activity_rules);
+        SharedPreferences sharedPreferences = getSharedPreferences("currentUser", 0);
+        currentUserId = sharedPreferences.getLong("currentUser", 0);
+        ruleRecyclerView = findViewById(R.id.recycler_view_rules_activity_rules);
         populateList();
-        RecyclerView.Adapter ruleAdapter = new RuleAdapter(ruleList, this);
+        ruleAdapter = new RuleAdapter(ruleList, this);
         ruleRecyclerView.setAdapter(ruleAdapter);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         ruleRecyclerView.setLayoutManager(layoutManager);
 
         FloatingActionButton addButton = findViewById(R.id.floating_button_add);
-        addButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Clicked add", Toast.LENGTH_SHORT).show();
-            }
-        });
+        addButton.setOnClickListener(v -> openAddNewRuleDialog());
     }
 
     private void populateList() {
@@ -55,6 +60,34 @@ public class RulesActivity extends BaseDrawerActivity {
         Rule r2 = new Rule("Rule 2", "Description 2");
         ruleList.add(r1);
         ruleList.add(r2);
+    }
+
+    private void openAddNewRuleDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(RulesActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_add_rule, null);
+        EditText deviceNameEditText = view.findViewById(R.id.text_rule_name);
+        EditText deviceDescEditText = view.findViewById(R.id.text_rule_description);
+        builder.setView(view)
+                .setNegativeButton(getResources().getString(R.string.button_cancel), (dialogInterface, i) -> {
+                })
+                .setPositiveButton(getResources().getString(R.string.button_add), null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
+            String deviceName = deviceNameEditText.getText().toString();
+            String deviceDesc = deviceDescEditText.getText().toString();
+            if (deviceName.length() != 0 && deviceDesc.length() != 0) {
+                dbManager = new DatabaseManager(getApplicationContext());
+               // dbManager.addRule(deviceName, deviceDesc, currentUserId, System.currentTimeMillis());
+
+                // update device display
+                //new PopulateDevices(this, ruleRecyclerView).execute(currentUserId);
+                dialog.dismiss();
+            } else {
+                Toast.makeText(getApplicationContext(), "Please fill in rule data", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
--- app/src/main/java/com/has/adapters/RuleAdapter.java
+++ app/src/main/java/com/has/adapters/RuleAdapter.java
@@ -6,15 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.has.R;
+import com.has.data.DatabaseManager;
+import com.has.model.Actuator;
 import com.has.model.Rule;
 
 import java.util.List;
@@ -23,6 +27,8 @@ public class RuleAdapter extends RecyclerView.Adapter<RuleAdapter.RuleViewHolder
 
     private List<Rule> ruleList;
     private Context context;
+    private DatabaseManager databaseManager;
+
 
     public RuleAdapter(List<Rule> ruleList, Context context) {
         this.ruleList = ruleList;
@@ -45,16 +51,25 @@ public class RuleAdapter extends RecyclerView.Adapter<RuleAdapter.RuleViewHolder
             @Override
             public void onClick(View v) {
                 PopupMenu popupMenu = new PopupMenu(context, holder.popupMenu);
-                popupMenu.getMenu().add(Menu.NONE, R.id.edit, 1, "Edit");
-                popupMenu.getMenu().add(Menu.NONE, R.id.delete, 2, "Delete");
+                popupMenu.getMenu().add(Menu.NONE, R.id.add_sensor_and_actuator, 1, "Add sensor and actuator");
+                popupMenu.getMenu().add(Menu.NONE, R.id.edit, 2, "Edit");
+                popupMenu.getMenu().add(Menu.NONE, R.id.delete, 3, "Delete");
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         switch (item.getItemId()) {
-                            case R.id.share:
+                            case R.id.add_sensor_and_actuator:
+                                Toast.makeText(context, "Add sensor and actuator", Toast.LENGTH_LONG).show();
+                                break;
+                            case R.id.edit:
+                                openEditActuatorDialog(rule, position);
                                 Toast.makeText(context, "Edit", Toast.LENGTH_LONG).show();
                                 break;
                             case R.id.delete:
+                                databaseManager = new DatabaseManager(context);
+                                databaseManager.deleteDevice(rule.getId());
+                                ruleList.remove(position);
+                                notifyItemRemoved(position);
                                 Toast.makeText(context, "Delete", Toast.LENGTH_LONG).show();
                                 break;
                         }
@@ -84,5 +99,42 @@ public class RuleAdapter extends RecyclerView.Adapter<RuleAdapter.RuleViewHolder
             description = itemView.findViewById(R.id.text_description_list);
             popupMenu = itemView.findViewById(R.id.button_popup_menu);
         }
+    }
+
+    private void openEditActuatorDialog(Rule actuator, int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.dialog_add_rule, null);
+        TextView title = view.findViewById(R.id.text_device_title);
+        title.setText(R.string.edit_rule);
+        EditText deviceNameEditText = view.findViewById(R.id.text_rule_name);
+        deviceNameEditText.setText(actuator.getName());
+        EditText deviceDescEditText = view.findViewById(R.id.text_rule_description);
+        deviceDescEditText.setText(actuator.getDescription());
+     /*   EditText deviceValueEditText = view.findViewById(R.id.text_device_value);
+        deviceValueEditText.setText(actuator.getValue());*/
+        builder.setView(view)
+                .setNegativeButton(context.getResources().getString(R.string.button_cancel), (dialogInterface, i) -> {})
+                .setPositiveButton(context.getResources().getString(R.string.button_edit), null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
+            String deviceName = deviceNameEditText.getText().toString();
+            String deviceDesc = deviceDescEditText.getText().toString();
+            //String value = deviceValueEditText.getText().toString();
+            if (deviceName.length() != 0 && deviceDesc.length() != 0) {
+                //dbManager.updateActuator(actuator.getId(), deviceName, deviceDesc, actuator.getDevice().getId(), value);
+
+                //update actuator
+                Rule updatedActuator = ruleList.get(position);
+                updatedActuator.setName(deviceName);
+                updatedActuator.setDescription(deviceDesc);
+                //updatedActuator.setValue(value);
+                notifyItemChanged(position);
+                dialog.dismiss();
+            } else {
+                Toast.makeText(context.getApplicationContext(), "Please fill in actuator data", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
